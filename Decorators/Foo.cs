5698106be3d6f292ab7c2fb51e7b1762b3f5980b
Jennifer Valle <jvs1192@gmail.com>
2016-04-02 13:26:51 +0200

    First Commit

---
+++ Decorators/Foo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+//añadir en referencias: System.ComponentModel.DataAnnotations(Botón derecho en proyecto=>añadir referencia)
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorators
+{
+    public class Foo
+    {   
+  //      using(var foo=new Foo()){
+  //          Console.WriteLine("Hello");
+     //   }
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(10, ErrorMessage="La longitud del nombre no es correcta")]
+       
+        public string Name { get; set; }
+
+    
+    }
+}
---
+++ Decorators/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo() { Name = "01234567891" };
+            var propierty = foo.GetType().GetProperty("Name");
+
+            var attributes=propierty.GetCustomAttributes(true);
+
+           // foreach (var atributos in attributes) {
+                
+                /* así no se puede hacer el cast.....
+                 * 
+                 * var attr=(MaxLengthAttribute)atributos
+                 * 
+                 * porque si sale required el código no compila.*/
+
+
+                /*Evitar esto: por alto rendimiento:
+                 * 
+                 * if(attribute is MaxLengthAttribute){
+                 *  var attr=(MaxLengthAttribute)attribute;
+                 * }
+                 * 
+                 * 
+                /*Éste es correcto en cuanto a rendimiento y robustez*/
+             /* var attr = atributos as MaxLengthAttribute; //si atributo es Maxlength lo pone a la izquierda, si no lo es, es null.
+            //Es hacer un cast sin romper código.
+               
+            if (null != attr) { 
+                 var value = propierty.GetValue(foo)
+              * if ((value != null) && value.ToString().Length > attr.Length) {
+                    throw new Exception(attr.ErrorMessage);
+                 }
+             }*/
+
+
+                /*Forma aun más eficiente y corta y para cualquier decorador, para cualquier validation*/
+
+                var value=propierty.GetValue(foo);
+                foreach(var attribute in attributes){
+                    var attr=attribute as ValidationAttribute;
+                    if(null!=attr){
+                        if(!attr.IsValid(value)){
+                            throw new Exception(attr.ErrorMessage);
+                        }
+                    }
+                }
+            }
+        }
+    
+}
